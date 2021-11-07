a262dec872f9ed91f55309d576d7c19eec7e0dc8
Luis-RasconCalderon <87144287+Luis-RasconCalderon@users.noreply.github.com>
2021-11-06 23:48:26 -0600

    Definición de atributos y métodos para la clase Producto
    Se crearon y definieron los atributos y los métodos (todos getters) de la clase de Producto. Esta clase heredará estos a sus clases hijas.

--- Producto.java
+++ Producto.java
@@ -1,3 +1,30 @@
 public class Producto {
+    private String precio;
+    private String serie;
+    private String marca;
+    private String fechaFab;
+    private String marcadorAR;
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getFechaFab() {
+        return fechaFab;
+    }
+
+    public String getMarcadorAR() {
+        return marcadorAR;
+    }
+
     
+
 }
